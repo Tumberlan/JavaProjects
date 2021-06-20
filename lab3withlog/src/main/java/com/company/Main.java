a058d481221f336c40d9962fefa57477babe4dc1
i.ashikhmin <i.ashikhmin@g.nsu.ru>
2021-06-20 16:47:38 +0700

    20.06.2021 16:47

--- lab3withlog/src/main/java/com/company/Main.java
+++ lab3withlog/src/main/java/com/company/Main.java
@@ -4,9 +4,22 @@ import javax.swing.*;
 
 public class Main{
     public static void main(String[] args) {
-
-        Multiplayer MP = new Multiplayer();
-        MP.InitServer("localhost", 22222);
+        boolean online = false;
+        boolean isDriver = true;
+        online = true;
+        if(online) {
+            if(isDriver) {
+                Multiplayer MP = new Multiplayer();
+                MP.InitServer("localhost", 22222);
+            }else{
+                Multiplayer MP = new Multiplayer();
+                MP.connect("localhost", 22222);
+            }
+        }else{
+            RoadLogic roadLogic = new RoadLogic(false);
+            RaceLayout RL = new RaceLayout();
+            RL.StartLayout(1, roadLogic);
+        }
         /*FirstLayout FL = new FirstLayout();
         FL.StartLayout();*/
 
