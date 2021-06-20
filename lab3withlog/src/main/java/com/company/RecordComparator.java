44be2a7c96055290b2538923d6067d4d3dcd1a26
i.ashikhmin <i.ashikhmin@g.nsu.ru>
2021-06-20 20:35:31 +0700

    20:35 20.06.2021

--- lab3withlog/src/main/java/com/company/RecordComparator.java
+++ lab3withlog/src/main/java/com/company/RecordComparator.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class RecordComparator implements Comparator<RecordTable.Record> {
     @Override
     public int compare(RecordTable.Record r1, RecordTable.Record r2){
-        return r1.value.compareTo(r2.value);
+        return r2.value.compareTo(r1.value);
     }
 }
