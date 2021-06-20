c500a508fabef372df9fc217ff1494ab5909f97d
i.ashikhmin <i.ashikhmin@g.nsu.ru>
2021-06-20 16:37:21 +0700

    20.06.2021 16:37

--- lab3withlog/src/main/java/com/company/Multiplayer.java
+++ lab3withlog/src/main/java/com/company/Multiplayer.java
@@ -3,12 +3,9 @@ package com.company;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
 
 public class Multiplayer extends Thread{
     private String ip = "localHost";
